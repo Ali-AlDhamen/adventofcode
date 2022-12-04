dd10db39adc941875fffbe8cd040a1c9f1c19236
Pali2002 <ali77dhamen@hotmail.com>
2022-12-04 19:20:03 +0300

    day4

---
+++ day4/day4.py
@@ -0,0 +1,24 @@
+
+
+total = 0
+
+with open('day4.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        words = line.split(",")
+        first_pair = tuple(words[0].split("-"))
+        second_pair = tuple(words[1].split("-"))
+        range2 = list(range(int(second_pair[0]) ,int(second_pair[1])+1))
+        if int(first_pair[0]) <= int(second_pair[0]) and int(first_pair[1]) >= int(second_pair[1]):
+            total +=1
+        elif int(first_pair[0]) >= int(second_pair[0]) and int(first_pair[1]) <= int(second_pair[1]):
+            total +=1
+        # this else if for 2nd part
+        elif int(first_pair[0]) in range2 or int(first_pair[1]) in range2:
+            total +=1
+
+
+print(total)
+
+
