995b2c4069bdb4aba87474cf87e51ea169c7118d
Ali-AlDhamen <ali77dhamen@hotmail.com>
2024-12-02 09:08:21 +0300

    day 2 of 2024 advent of code

---
+++ 2024/day2/p2.py
@@ -0,0 +1,28 @@
+def check_is_safe(arr):     
+    increasing = arr[0] < arr[1]
+    
+    for i in range(len(arr)-1):
+        if (abs(arr[i] - arr[i+1]) < 1 or abs(arr[i] - arr[i+1]) > 3):
+            break
+        elif (increasing and arr[i] > arr[i+1]) or (not increasing and arr[i] < arr[i+1]):
+            break
+    else:
+        return True
+    return False
+
+with open("in.txt", 'r') as f:
+    data = f.read().splitlines()
+    safe = 0
+    
+    for line in data:
+        arr = list(map(int, line.split(" ")))
+        if check_is_safe(arr):
+            safe += 1
+        else:   
+            for i in range(len(arr)):
+                dampened = arr[:i] + arr[i+1:]
+                if check_is_safe(dampened):
+                    safe += 1
+                    break  
+
+    print(safe)
